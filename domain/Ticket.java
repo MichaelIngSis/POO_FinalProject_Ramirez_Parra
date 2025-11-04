a82fe4ed85e45df0a9562dd9603e47d2c5a23c8d
USUARIO <michael3135056851@gmail.com>
2025-11-03 20:53:46 -0500

    Se agregan los atributos y los metodos generales de las clases Ticket y TicketOffice

--- domain/Ticket.java
+++ domain/Ticket.java
@@ -1,3 +1,16 @@
 public class Ticket {
-    
+    private int ticketNumber;
+    private int ticketSeat;
+    private String ticketLocation;
+    private Customer customer;
+
+    public Ticket(int ticketNumber, int ticketSeat, String ticketLocation){
+        this.ticketNumber = ticketNumber;
+        this.ticketSeat = ticketSeat;
+        this.ticketLocation = ticketLocation;
+    }
+
+    public int getTicketNumber(){return ticketNumber;}
+    public int ticketSeat(){return ticketSeat;}
+    public String ticketLocation(){return ticketLocation;}
 }
--- domain/TicketOffice.java
+++ domain/TicketOffice.java
@@ -1,3 +1,50 @@
+import java.util.List;
+
 public class TicketOffice {
-    
+    private int ticketOfficeNit;
+    private String ticketOfficeAddress;
+    private String ticketOfficeEmail;
+    private String ticketOfficePhoneNumber;
+    private String ticketOfficeCity;
+    private List<Customer> customers;
+    private List<Event> events;
+
+    public TicketOffice(int ticketOfficeNit, String ticketOfficeAddress, String ticketOfficeEmail, String ticketOfficePhoneNumber,
+            String ticketOfficeCity){
+        this.ticketOfficeNit = ticketOfficeNit;
+        this.ticketOfficeAddress = ticketOfficeAddress;
+        this.ticketOfficeEmail = ticketOfficeEmail;
+        this.ticketOfficePhoneNumber = ticketOfficePhoneNumber;
+        this.ticketOfficeCity = ticketOfficeCity;
+    }
+
+    public void setTicketOfficeAddress(String newTicketOfficeAddress){
+        if(!newTicketOfficeAddress.equals("")){
+            ticketOfficeAddress = newTicketOfficeAddress;
+        }
+    }
+
+    public void setTicketOfficeEmail(String newTicketOfficeEmail){
+        if(!newTicketOfficeEmail.equals("")){
+            ticketOfficeEmail = newTicketOfficeEmail;
+        }
+    }
+
+    public void setTicketOfficePhoneNumber(String newTicketOfficePhoneNumber){
+        if(!newTicketOfficePhoneNumber.equals("")){
+            ticketOfficePhoneNumber = newTicketOfficePhoneNumber;
+        }
+    }
+
+    public void setTicketOfficeCity(String newTicketOfficeCity){
+        if(!newTicketOfficeCity.equals("")){
+            ticketOfficeCity = newTicketOfficeCity;
+        }
+    }
+
+    public int getTicketOfficeNit(){return ticketOfficeNit;}
+    public String getTicketOfficeAddress(){return ticketOfficeAddress;}
+    public String getTicketOfficeEmail(){return ticketOfficeEmail;}
+    public String getTicketOfficePhoneNumber(){return ticketOfficePhoneNumber;}
+    public String getTicketOfficeCity(){return ticketOfficeCity;}
 }
